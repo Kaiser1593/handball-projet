eedffbe5a8043f6ce847f33c8a42e125e5ed3865
Kaiser1593 <kaispadrines93@gmail.com>
2025-05-29 21:13:58 +0200

    v2

--- php/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=handball;charset=utf8", "root", "");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
-?>
